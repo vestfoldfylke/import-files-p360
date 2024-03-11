1503a8fd9e5cc09ab081e6812638e1bc143964ed
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2024-03-11 14:43:13 +0100

    nununu

--- config.js
+++ config.js
@@ -6,6 +6,13 @@ module.exports = {
     BARCODE_IMPORTED_DIR: process.env.BARCODE_IMPORTED_DIR,
     BARCODE_IMPORTED_TO_UNREG_DIR: process.env.BARCODE_IMPORTED_TO_UNREG_DIR
   },
+  UNREGISTERED: {
+    INPUT_DIR: process.env.UNREGISTERED_INPUT_DIR,
+    IMPORTED_DIR: process.env.UNREGISTERED_IMPORTED_DIR,
+    FAILED_DIR: process.env.UNREGISTERED_FAILED_DIR,
+    GET_AD_USER: process.env.UNREGISTERED_GET_AD_USER,
+    AD_MAIN_COUNTY_OU: process.env.UNREGISTERED_AD_MAIN_COUNTY_OU
+  },
   P360: {
     URL: process.env.P360_URL || 'fjdsoijfidsf.vtfk.no',
     AUTHKEY: process.env.P360_AUTHKEY || 'fjdsoijfidsf.vtfk.no',
